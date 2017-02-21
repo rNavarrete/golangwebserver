60cfead2aad0e42ad010e17874f04a1a3bf2e9b4
Rolando Navarrete <kinglouis.belmont@gmail.com>
2017-02-21 08:20:48 -0800

    finished up the save handler

--- wiki.go
+++ wiki.go
@@ -63,7 +63,8 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/save/"):]
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	p.Save()
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
