0cf61bfac202ab86ef13a384181bbcab8496639b
Rolando Navarrete <rolando@splice.com>
2017-02-21 06:30:43 -0800

    added view and edit handlers

--- wiki.go
+++ wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 )
@@ -26,14 +26,39 @@ func loadPage(title string) (*Page, error) {
 }
 
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	//http.HandleFunc("/save/", saveHandler)
+	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	// p1.save()
+	// p2, _ := loadPage("TestPage")
+	// fmt.Println(string(p2.Body))
+	// http.HandleFunc("/view/", viewHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
+	// capture the title from the http request
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	fmt.Fprint(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	// use the captured title from the request to load the page
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	// render the appropriate template by passing in the "view" variable and the page.
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
 }
